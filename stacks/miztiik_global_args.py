7e3ad50d6043f879b0e5cec55e829a82c67060c4
Mystique <13oct08@quantumfoam.uni.cc>
2021-07-31 18:56:38 +0200

    LGTM

--- stacks/miztiik_global_args.py
+++ stacks/miztiik_global_args.py
@@ -6,5 +6,5 @@ class GlobalArgs():
     OWNER = "MystiqueAutomation"
     REPO_NAME = "eks-security-with-network-policies"
     SOURCE_INFO = f"https://github.com/miztiik/{REPO_NAME}"
-    VERSION = "2020-06-20"
+    VERSION = "2021-06-20"
     MIZTIIK_SUPPORT_EMAIL = ["mystique@example.com", ]
